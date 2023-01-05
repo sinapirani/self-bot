2eb4f8deda2b791f3581c0333f68f972cb5b03a0
sinapirani <realsinapirani@gmail.com>
2023-01-05 20:00:37 +0330

    📦: Add express

--- src/app.ts
+++ src/app.ts
@@ -2,52 +2,53 @@ import { Api, TelegramClient } from "telegram";
 import { StringSession } from "telegram/sessions";
 import { updateBio } from "../modules/updateBio";
 const input = require("input"); // npm i input
-import dotenv from 'dotenv'
+import dotenv from "dotenv";
 import { sayHello } from "../modules/sayHello";
 import { MongoClient } from "mongodb";
 import { env } from "process";
 import { getNewMessages } from "../events/events";
 // import { S3Client } from "@aws-sdk/client-s3";
-import AWS from 'aws-sdk'
+import AWS from "aws-sdk";
+import express, { Request } from "express";
+const app = express();
 
 // import { mongoClient } from "../database/db";
 dotenv.config();
 
 const apiId = +process.env.API_ID!;
 const apiHash = process.env.API_HASH!;
-const stringSession = new StringSession(
-  process.env.SESSION!
-); // fill this later with the value from session.save()
+const stringSession = new StringSession(process.env.SESSION!); // fill this later with the value from session.save()
 export const mongoClient = new MongoClient(env.MONGODB_URI!);
 export const Database = {
   users: mongoClient.db(env.DB_NAME).collection("users"),
-  messages: mongoClient.db(env.DB_NAME).collection("messages")
-}
-
+  messages: mongoClient.db(env.DB_NAME).collection("messages"),
+};
 
 export const S3Client = new AWS.S3({
   endpoint: process.env.LIARA_ENDPOINT,
-  credentials:{
+  credentials: {
     accessKeyId: process.env.LIARA_ACCESS_KEY!,
-    secretAccessKey: process.env.LIARA_SECRET_KEY!
-  }
-})
-
-export const client = new TelegramClient(stringSession, apiId, apiHash, {
-  connectionRetries: 5, 
+    secretAccessKey: process.env.LIARA_SECRET_KEY!,
+  },
 });
 
-
+export const client = new TelegramClient(stringSession, apiId, apiHash, {
+  connectionRetries: 5,
+});
 
 (async () => {
-    
-    await mongoClient.connect()
-    await client.connect() 
+  await mongoClient.connect();
+  await client.connect();
 
-    // setInterval(async() => {
-    //   await updateBio()
-    // }, 315000);
-    getNewMessages()
-    
-  
+  // setInterval(async() => {
+  //   await updateBio()
+  // }, 315000);
+  getNewMessages();
 })();
+
+app.get("/", async (req: Request, res: Response) => {
+  await res.json({
+    success: true,
+  });
+});
+app.listen(process.env.PORT || 3000);
