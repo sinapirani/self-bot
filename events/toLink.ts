af9451398e1a957ff8d7b9e2486cf045e415f714
sinapirani <realsinapirani@gmail.com>
2023-02-15 13:55:53 +0330

    📦: debuged and updated

--- events/toLink.ts
+++ events/toLink.ts
@@ -4,10 +4,7 @@ import fs from "fs";
 import { ManagedUpload, PutObjectRequest } from "aws-sdk/clients/s3";
 import { TEXT_GENERATOR } from "../texts/texts";
 import memetype from "mime-types";
-import {BigInteger} from "big-integer"
-
-
-
+import { BigInteger } from "big-integer";
 
 export const toLinkCommand = async (event: NewMessageEvent) => {
   const message = event.message;
@@ -18,66 +15,89 @@ export const toLinkCommand = async (event: NewMessageEvent) => {
 
   //exist when command not same
   if (message.text != "~toLink") return;
-  if(senderId?.toJSNumber() != process.env.ADMIN) return;
+  if (senderId?.toJSNumber() != process.env.ADMIN) return;
 
-  const repliedMessages = await client.getMessages(chatid, {
-    ids: replyToMsgId,
-  });
-  const repliedMessage = repliedMessages[0];
-  const repliedMessageMedia = repliedMessage.media!;
-
-  let fileExt;
-  //@ts-ignore
-  let photoInfo = repliedMessageMedia?.photo;
-  //@ts-ignore
-  let documentInfo = repliedMessageMedia?.document;
-
-  if (photoInfo) {
-    fileExt = "jpg";
-  }
-  if (documentInfo) {
-    fileExt = memetype.extension(documentInfo.mimeType);
-  }
-  
   try {
-
-    const chat = chatid
-    let progressPercent = 0;
-    let tempProgressPercent = 0;
-    let startMessage = await client.sendMessage(chatid!, {
-      message: "Download Started ... ",
-      replyTo: message.replyToMsgId,
+    const repliedMessages = await client.getMessages(chatid, {
+      ids: replyToMsgId,
     });
-    console.log('start Massage', startMessage);
-    let uploadText;
-    const progress = async(recived: BigInteger, total: BigInteger) => {
-      progressPercent = Math.floor(+recived.toString() / +total.toString()* 100) ;
-      console.log('progrece percent', progressPercent);
+
+    for (let repliedMessage of repliedMessages) {
+      const repliedMessageMedia = repliedMessage.media!;
+      console.log("repliedMessageMedia", repliedMessageMedia);
       
-      if(progressPercent == tempProgressPercent) return
-      uploadText = `Upload To Server: ${progressPercent}% 🚀`;
-      await client.editMessage(chat!, {
-        message: startMessage.id,
-        text: uploadText,
+
+      let fileExt;
+      //@ts-ignore
+      let photoInfo = repliedMessageMedia?.photo;
+      //@ts-ignore
+      let documentInfo:{mimeType:string, attributes:object[any]} = repliedMessageMedia?.document;
+
+      if (photoInfo) {
+        fileExt = "jpg";
+      }
+      if (documentInfo) { 
+        fileExt = memetype.extension(documentInfo.mimeType);
+      }
+      
+      
+
+      const chat = chatid;
+      let progressPercent = 0;
+      let tempProgressPercent = 0;
+
+
+      let startMessage = await client.sendMessage(chatid!, {
+        message: "Download Started ... ",
+        replyTo: message.replyToMsgId,
       });
-      tempProgressPercent = progressPercent
-    };
+      // console.log("start Massage", startMessage);
 
-    let fileBuffer = await client.downloadMedia(repliedMessage?.media!, {
-      progressCallback: progress,
-    });
-    const fileKey = `${Math.floor(Math.random() * 432525231)}.${fileExt}`;
-    const s3ObjParams: PutObjectRequest = {
-      Body: fileBuffer,
-      Bucket: process.env.LIARA_BUCKET_NAME!,
-      Key: fileKey,
-    };
 
-    await S3Client.putObject(s3ObjParams).promise();
-    await client.editMessage(chatid!, { 
-      message: startMessage.id,
-      text: TEXT_GENERATOR.downloadLinkText(uploadText, fileKey),
-    });
+      let uploadText;
+      const progress = async (recived: BigInteger, total: BigInteger) => {
+        
+        progressPercent = Math.floor(
+          (+recived.toString() / +total.toString()) * 100
+        );
+        console.log("progrece percent", progressPercent);
+
+        if (progressPercent == tempProgressPercent) return;
+        uploadText = `Upload To Server: ${progressPercent}% 🚀`;
+        await client.editMessage(chat!, {
+          message: startMessage.id,
+          text: uploadText,
+        });
+        tempProgressPercent = progressPercent;
+        
+      };
+ 
+      let fileBuffer = await client.downloadMedia(repliedMessage?.media!, {
+        progressCallback: progress,
+      });
+      
+      let documentFileName = documentInfo?.attributes.find((e:any) => e.fileName)
+      // if(!documentFileName){
+      //   return message.reply({
+      //     message: "Cant Read file name!"
+      //   })
+      // }
+      documentFileName = documentFileName ? documentFileName.fileName : `${(Math.random() * 201314)}`
+      console.log('documentFileName', documentFileName);
+      
+      const fileKey = `${ documentFileName + Math.floor(Math.random() *10)}.${fileExt}`;
+      const s3ObjParams: PutObjectRequest = {
+        Body: fileBuffer,
+        Bucket: process.env.LIARA_BUCKET_NAME!,
+        Key: fileKey,
+      };
+
+      await S3Client.putObject(s3ObjParams).promise()
+      await client.editMessage(chatid!, {
+        message: startMessage.id,
+        text: TEXT_GENERATOR.downloadLinkText(uploadText, fileKey),
+      });
+    }
   } catch (e) {
     console.log(e);
   }
--- tempCodeRunnerFile.javascript
+++
@@ -1,4 +0,0 @@
-
-
-const command = "~admins add full"
-console.log(command.split(" "));
